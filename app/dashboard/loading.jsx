900803134d1b227704ab34f9f5a1eb35054a26ca
Siratul Islam <islamsiratul@gmail.com>
2023-12-02 11:03:36 +0600

    grit_gym

--- app/dashboard/loading.jsx
+++ app/dashboard/loading.jsx
@@ -2,13 +2,13 @@ const loading = () => {
   return (
     <>
       <div className="flex justify-center bg-slate-50 ">
-        <div className="py-[150px] ">
-          <div className="flex flex-col gap-6 w-52">
-            <div className="skeleton h-4 w-[300px]"></div>
-            <div className="skeleton h-4 w-[300px]"></div>
-            <div className="skeleton h-4 w-[300px]"></div>
-            <div className="skeleton h-4 w-[300px]"></div>
-            <div className="skeleton h-4 w-[300px]"></div>
+        <div className="py-[300px] sm:py-[150px]  ">
+          <div className="flex flex-col gap-6 w-42 sm:w-52 ">
+            <div className="skeleton h-4 w-[300px] bg-slate-300 "></div>
+            <div className="skeleton h-4 w-[300px] bg-slate-300 "></div>
+            <div className="skeleton h-4 w-[300px] bg-slate-300 "></div>
+            <div className="skeleton h-4 w-[300px] bg-slate-300 "></div>
+            <div className="skeleton h-4 w-[300px] bg-slate-300 "></div>
           </div>
         </div>
       </div>
