b024e1ec539028dfd1eb96af9a81bfe37d8dcb0c
Siratul Islam <islamsiratul@gmail.com>
2024-03-19 12:39:05 +0600

    grit_gym_main

--- app/ui/Global/Sidebar/CollapsedSidebar.jsx
+++ app/ui/Global/Sidebar/CollapsedSidebar.jsx
@@ -6,13 +6,18 @@ import { FaUserEdit } from "react-icons/fa";
 import { FaUserCog } from "react-icons/fa";
 import { FaListUl } from "react-icons/fa";
 import { CgGym } from "react-icons/cg";
+import { FaUserLock } from "react-icons/fa";
+import { FaUserFriends } from "react-icons/fa";
 //gym
 import { FaPeopleGroup } from "react-icons/fa6";
 import { LuPackageCheck } from "react-icons/lu";
 import { TbSettings } from "react-icons/tb";
+import { useState } from "react";
 
 const CollapsedSidebar = ({ data }) => {
   const pathname = usePathname();
+  const [isHovering, setIsHovering] = useState(false);
+
   return (
     <>
       <div className="font-sans ">
@@ -99,19 +104,49 @@ const CollapsedSidebar = ({ data }) => {
               </Link>
             </li>
             <li className="pl-3 pr-3 pt-3">
-              <Link href="/dashboard/gym/setting">
+              <button onClick={() => setIsHovering(!isHovering)}>
                 <span
                   className={
-                    pathname === "/dashboard/gym/setting"
+                    pathname.includes("/user") || pathname.includes("/role")
                       ? " font-bold p-3 bg-[#00a76f14] text-md rounded-lg text-[#00a76f] flex"
                       : " font-bold p-3 hover:bg-[#00a76f14] text-md rounded-lg text-[black] flex"
                   }
                 >
                   <span className="pl-1  text-sm flex ">
+                    {" "}
                     <TbSettings size={20} />
                   </span>
                 </span>
-              </Link>
+              </button>
+            </li>
+            <li className="">
+              {isHovering && (
+                <ul className="absolute left-3 bg-white border border-gray-300 p-2 rounded shadow">
+                  <Link href="/dashboard/gym/setting/user">
+                    <li
+                      className={
+                        pathname === "/dashboard/gym/setting/user"
+                          ? "bg-gray-100 cursor-pointer text-black p-2 "
+                          : "hover:bg-gray-100 cursor-pointer text-black p-2 "
+                      }
+                    >
+                      <FaUserFriends size={16} />
+                    </li>
+                  </Link>
+                  <div className="py-1"></div>
+                  <Link href="/dashboard/gym/setting/role">
+                    <li
+                      className={
+                        pathname === "/dashboard/gym/setting/role"
+                          ? "bg-gray-100 cursor-pointer text-black p-2 "
+                          : "hover:bg-gray-100 cursor-pointer text-black p-2 "
+                      }
+                    >
+                      <FaUserLock size={16} />
+                    </li>
+                  </Link>
+                </ul>
+              )}
             </li>
           </>
         )}
