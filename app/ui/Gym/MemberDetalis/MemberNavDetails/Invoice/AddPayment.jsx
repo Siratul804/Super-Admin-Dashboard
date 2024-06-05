e0c0e219821f1f257d0aaab19393a1c0c9c2b273
Siratul Islam <islamsiratul@gmail.com>
2024-06-05 11:27:11 +0600

    grit_gym_main

--- app/ui/Gym/MemberDetalis/MemberNavDetails/Invoice/AddPayment.jsx
+++ app/ui/Gym/MemberDetalis/MemberNavDetails/Invoice/AddPayment.jsx
@@ -2,16 +2,20 @@
 import { addPayment } from "@/app/lib/actions";
 import { useFormState, useFormStatus } from "react-dom";
 import toast from "react-hot-toast";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { FaMoneyCheckDollar } from "react-icons/fa6";
 
-const AddPayment = async ({ inv_id, user, inv_amount }) => {
+const AddPayment = ({ inv_id, user, inv_amount }) => {
+  const [discount, setDiscount] = useState(0);
+  const [discountType, setDiscountType] = useState("amount"); // 'amount' or 'percentage'
+  const [amountToPay, setAmountToPay] = useState(0);
+
   function Submit() {
     const { pending } = useFormStatus();
     return (
       <button
         type="submit"
-        className="btn btn-sm btn-neutral text-white h-[6vh] w-[35vh] rounded-md "
+        className="btn btn-sm btn-neutral text-white h-[6vh] w-[35vh] rounded-md"
         disabled={pending}
       >
         {pending ? "Saving..." : "Make Payment"}
@@ -47,11 +51,29 @@ const AddPayment = async ({ inv_id, user, inv_amount }) => {
     }
   }, [state]);
 
+  const dueAmount = inv_amount;
+
+  useEffect(() => {
+    const calculateDiscount = () => {
+      let finalAmount = 0;
+
+      if (discountType === "amount") {
+        finalAmount = dueAmount - discount;
+      } else if (discountType === "percentage") {
+        finalAmount = dueAmount - dueAmount * (discount / 100);
+      }
+
+      setAmountToPay(finalAmount > 0 ? finalAmount : 0); // Ensure amount to pay is not negative
+    };
+
+    calculateDiscount();
+  }, [dueAmount, discount, discountType]);
+
   return (
     <div>
-      <div className="flex justify-start ">
+      <div className="flex justify-start">
         <button
-          className="p-2 hover:bg-slate-100 rounded-full "
+          className="p-2 hover:bg-slate-100 rounded-full"
           onClick={() => document.getElementById(inv_id).showModal()}
         >
           <FaMoneyCheckDollar size={20} />
@@ -59,16 +81,16 @@ const AddPayment = async ({ inv_id, user, inv_amount }) => {
       </div>
       {/* .....add modal data....... */}
       <dialog id={inv_id} className="modal">
-        <div className="modal-box bg-white max-w-[80vh] ">
+        <div className="modal-box bg-white max-w-[80vh]">
           <div className="">
             {/* //inside content// */}
             <div className="">
-              <div className="flex justify-between ">
-                <h1 className="text-xl  text-black ">Make Payment</h1>
+              <div className="flex justify-between">
+                <h1 className="text-xl text-black">Make Payment</h1>
                 <div>
                   <form method="dialog">
                     {/* if there is a button in form, it will close the modal */}
-                    <button className="btn btn-sm btn-circle btn-ghost absolute right-4  text-black   ">
+                    <button className="btn btn-sm btn-circle btn-ghost absolute right-4 text-black">
                       ✕
                     </button>
                   </form>
@@ -88,7 +110,7 @@ const AddPayment = async ({ inv_id, user, inv_amount }) => {
                       value={user.name}
                     />
                   </div>
-                  <div className="flex justify-between sm:flex-row flex-col  ">
+                  <div className="flex justify-between sm:flex-row flex-col">
                     <main className="pr-1">
                       <label className="label">
                         <span className="text-[black] text-sm">
@@ -100,13 +122,13 @@ const AddPayment = async ({ inv_id, user, inv_amount }) => {
                         placeholder="Due Amount"
                         name="amount_due"
                         required
-                        autocomplete="off"
-                        value={inv_amount}
-                        // className="input input-sm  bg-white  text-black border-black focus:outline-black focus:border-black w-[35vh]"
-                        className=" input  h-[6vh] bg-[#FFFFFF] appearance-none border-1 border-[#8d94b0] rounded-md w-[35vh] py-2 px-4 text-black leading-tight focus:outline-none focus:bg-white focus:border-black"
+                        autoComplete="off"
+                        value={dueAmount}
+                        className="input h-[6vh] bg-[#FFFFFF] appearance-none border-1 border-[#8d94b0] rounded-md w-[35vh] py-2 px-4 text-black leading-tight focus:outline-none focus:bg-white focus:border-black"
+                        readOnly
                       />
                     </main>
-                    <main className="pr-1  ">
+                    <main className="pr-1">
                       <label className="label">
                         <span className="text-[black] text-sm">
                           Discount Type
@@ -114,33 +136,34 @@ const AddPayment = async ({ inv_id, user, inv_amount }) => {
                       </label>
                       <select
                         name="dis_type"
-                        className=" h-[6vh] bg-[#FFFFFF] appearance-none border-[1px] border-[#8d94b0] rounded-md w-[35vh] py-1 px-4 text-black leading-tight focus:outline-none focus:bg-white focus:border-black"
+                        value={discountType}
+                        onChange={(e) => setDiscountType(e.target.value)}
+                        className="h-[6vh] bg-[#FFFFFF] appearance-none border-[1px] border-[#8d94b0] rounded-md w-[35vh] py-1 px-4 text-black leading-tight focus:outline-none focus:bg-white focus:border-black"
                       >
-                        <option disabled selected>
+                        <option disabled value="">
                           Select
                         </option>
-                        <option>Amount</option>
-                        <option>Percentage</option>
+                        <option value="amount">Amount</option>
+                        <option value="percentage">Percentage</option>
                       </select>
                     </main>
                   </div>
 
-                  <div className="flex justify-between sm:flex-row flex-col  ">
+                  <div className="flex justify-between sm:flex-row flex-col">
                     <main>
                       <label className="label">
-                        <span className="text-[black] text-sm ">
-                          {" "}
-                          Discount{" "}
-                        </span>
+                        <span className="text-[black] text-sm">Discount</span>
                       </label>
                       <input
                         type="number"
                         placeholder="Discount"
                         name="discount"
-                        defaultValue="0.00"
-                        required
-                        autocomplete="off"
-                        className=" input  h-[6vh] bg-[#FFFFFF] appearance-none border-1 border-[#8d94b0] rounded-md w-[35vh] py-2 px-4 text-black leading-tight focus:outline-none focus:bg-white focus:border-black"
+                        value={discount}
+                        onChange={(e) =>
+                          setDiscount(parseFloat(e.target.value))
+                        }
+                        autoComplete="off"
+                        className="input h-[6vh] bg-[#FFFFFF] appearance-none border-1 border-[#8d94b0] rounded-md w-[35vh] py-2 px-4 text-black leading-tight focus:outline-none focus:bg-white focus:border-black"
                       />
                     </main>
                     <main className="pr-1">
@@ -149,9 +172,9 @@ const AddPayment = async ({ inv_id, user, inv_amount }) => {
                       </label>
                       <select
                         name="pay_type"
-                        className=" h-[6vh] bg-[#FFFFFF] appearance-none border-[1px] border-[#8d94b0] rounded-md w-[35vh] py-1 px-4 text-black leading-tight focus:outline-none focus:bg-white focus:border-black"
+                        className="h-[6vh] bg-[#FFFFFF] appearance-none border-[1px] border-[#8d94b0] rounded-md w-[35vh] py-1 px-4 text-black leading-tight focus:outline-none focus:bg-white focus:border-black"
                       >
-                        <option disabled selected>
+                        <option disabled value="">
                           Select
                         </option>
                         <option>Cash</option>
@@ -160,25 +183,27 @@ const AddPayment = async ({ inv_id, user, inv_amount }) => {
                       </select>
                     </main>
                   </div>
-                  <div className="flex justify-between sm:flex-row flex-col  ">
+                  <div className="flex justify-between sm:flex-row flex-col">
                     <main className="pr-1">
                       <label className="label">
-                        <span className="text-[black] text-sm"> Amount</span>
+                        <span className="text-[black] text-sm">
+                          Amount to pay
+                        </span>
                       </label>
                       <input
                         type="number"
                         placeholder="Amount"
                         name="amount"
                         required
-                        autocomplete="off"
-                        defaultValue="0.00"
-                        // className="input input-sm  bg-white  text-black border-black focus:outline-black focus:border-black w-[35vh]"
-                        className=" input  h-[6vh] bg-[#FFFFFF] appearance-none border-1 border-[#8d94b0] rounded-md w-[35vh] py-2 px-4 text-black leading-tight focus:outline-none focus:bg-white focus:border-black"
+                        autoComplete="off"
+                        value={amountToPay.toFixed(2)}
+                        className="input h-[6vh] bg-[#FFFFFF] appearance-none border-1 border-[#8d94b0] rounded-md w-[35vh] py-2 px-4 text-black leading-tight focus:outline-none focus:bg-white focus:border-black"
+                        readOnly
                       />
                     </main>
                     <main>
                       <label className="label">
-                        <span className="text-[black] text-sm pt-5 "></span>
+                        <span className="text-[black] text-sm pt-5"></span>
                       </label>
                       <Submit />
                     </main>
